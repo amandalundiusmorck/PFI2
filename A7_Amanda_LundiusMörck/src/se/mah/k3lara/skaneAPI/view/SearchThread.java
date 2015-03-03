9ea0470325d4bac4160210f047c55d44989aa630
Amanda <amandalundius@hotmail.com>
2015-03-03 16:36:52 +0100

    New Thread

---
+++ A7_Amanda_LundiusMörck/src/se/mah/k3lara/skaneAPI/view/SearchThread.java
@@ -0,0 +1,24 @@
+package se.mah.k3lara.skaneAPI.view;
+import java.util.Calendar;
+
+public class SearchThread extends Thread{
+	private GUI gui;
+	private boolean running = true;
+
+	public SearchThread(GUI g){
+		this.gui = g;
+	}
+
+	public void run() {
+		while(running==true){
+			try{
+				Thread.sleep(60000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			gui.UpdateLables();
+		}
+
+	}
+
+}
